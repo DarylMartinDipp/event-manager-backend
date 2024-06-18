798821ed148b1f3c4884bca00eeaf300719b023b
DarylMartinDipp <didieravangers@gmail.com>
2024-06-18 14:16:47 +0200

    Init backend project

---
+++ src/main/java/com/dauphine/event_management/EventManagementBackendApplication.java
@@ -0,0 +1,11 @@
+package com.dauphine.event_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementBackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementBackendApplication.class, args);
+	}
+}
