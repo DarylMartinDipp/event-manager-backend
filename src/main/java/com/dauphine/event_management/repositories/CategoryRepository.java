46f26d011b14f9bd33c6263ada2abc9a7dc68fe6
DarylMartinDipp <didieravangers@gmail.com>
2024-06-18 15:10:43 +0200

    Add the CategoryRepository interface

---
+++ src/main/java/com/dauphine/event_management/repositories/CategoryRepository.java
@@ -0,0 +1,18 @@
+package com.dauphine.event_management.repositories;
+
+import com.dauphine.event_management.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    @Query("""
+        SELECT c
+        FROM Category c
+        WHERE c.name LIKE CONCAT('%', :name, '%')
+    """)
+    List<Category> findAllByName(@Param("name") String name);
+}
