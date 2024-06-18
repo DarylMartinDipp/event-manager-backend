acee760aaefadbc56f0c06e6034c2fdcf1c0d069
DarylMartinDipp <didieravangers@gmail.com>
2024-06-18 15:16:17 +0200

    Clean code

--- src/main/java/com/dauphine/event_management/services/CategoryService.java
+++ src/main/java/com/dauphine/event_management/services/CategoryService.java
@@ -9,9 +9,14 @@ import java.util.UUID;
 
 public interface CategoryService {
     List<Category> getAll();
+
     List<Category> getAllByName(String name);
+
     Category getById(UUID id) throws CategoryNotFoundByIdException;
+
     Category create(String name) throws CategoryAlreadyExistsException;
+
     Category updateName(UUID id, String newName) throws CategoryNotFoundByIdException, CategoryAlreadyExistsException;
+
     void deleteById(UUID id) throws CategoryNotFoundByIdException;
 }
