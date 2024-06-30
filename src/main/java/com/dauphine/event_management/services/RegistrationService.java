1342c1408ee73e3331bde4cad44a1eb5173e093c
DarylMartinDipp <didieravangers@gmail.com>
2024-06-30 21:17:51 +0200

    Create the service associated to the Registration model and implement it

---
+++ src/main/java/com/dauphine/event_management/services/RegistrationService.java
@@ -0,0 +1,27 @@
+package com.dauphine.event_management.services;
+
+import com.dauphine.event_management.exceptions.event.EventNotFoundByIdException;
+import com.dauphine.event_management.exceptions.registration.RegistrationNotFoundByIdException;
+import com.dauphine.event_management.exceptions.user.UserNotFoundByIdException;
+import com.dauphine.event_management.models.Registration;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface RegistrationService {
+    List<Registration> getAllRegistrations();
+
+    Registration getRegistrationById(UUID registrationId) throws RegistrationNotFoundByIdException;
+
+    List<Registration> getRegistrationsByEventId(UUID eventId);
+
+    List<Registration> getRegistrationsByUserId(UUID userId);
+
+    Registration createRegistration(UUID userId, UUID eventId) throws UserNotFoundByIdException,
+            EventNotFoundByIdException;
+
+    Registration updateRegistration(UUID registrationId, UUID userId, UUID eventId)
+            throws RegistrationNotFoundByIdException, UserNotFoundByIdException, EventNotFoundByIdException;
+
+    void deleteRegistrationById(UUID registrationId) throws RegistrationNotFoundByIdException;
+}
---
+++ src/main/java/com/dauphine/event_management/services/impl/RegistrationServiceImpl.java
@@ -0,0 +1,80 @@
+package com.dauphine.event_management.services.impl;
+
+import com.dauphine.event_management.exceptions.event.EventNotFoundByIdException;
+import com.dauphine.event_management.exceptions.registration.RegistrationNotFoundByIdException;
+import com.dauphine.event_management.exceptions.user.UserNotFoundByIdException;
+import com.dauphine.event_management.models.Event;
+import com.dauphine.event_management.models.Registration;
+import com.dauphine.event_management.models.User;
+import com.dauphine.event_management.repositories.RegistrationRepository;
+import com.dauphine.event_management.services.EventService;
+import com.dauphine.event_management.services.RegistrationService;
+import com.dauphine.event_management.services.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class RegistrationServiceImpl implements RegistrationService {
+    private final RegistrationRepository registrationRepository;
+    private final EventService eventService;
+    private final UserService userService;
+
+    public RegistrationServiceImpl(RegistrationRepository registrationRepository, EventService eventService,
+                                   UserService userService) {
+        this.registrationRepository = registrationRepository;
+        this.eventService = eventService;
+        this.userService = userService;
+    }
+
+    @Override
+    public List<Registration> getAllRegistrations() {
+        return registrationRepository.findAll();
+    }
+
+    @Override
+    public Registration getRegistrationById(UUID registrationId) throws RegistrationNotFoundByIdException {
+        return registrationRepository.findById(registrationId).orElseThrow(
+                () -> new RegistrationNotFoundByIdException(registrationId)
+        );
+    }
+
+    @Override
+    public List<Registration> getRegistrationsByEventId(UUID eventId) {
+        return registrationRepository.findByEventId(eventId);
+    }
+
+    @Override
+    public List<Registration> getRegistrationsByUserId(UUID userId) {
+        return registrationRepository.findByUserId(userId);
+    }
+
+    @Override
+    public Registration createRegistration(UUID userId, UUID eventId) throws UserNotFoundByIdException, EventNotFoundByIdException {
+        User user = userService.getUserById(userId);
+        Event event = eventService.getEventById(eventId);
+
+        Registration newRegistration = new Registration(user, event);
+        return registrationRepository.save(newRegistration);
+    }
+
+    @Override
+    public Registration updateRegistration(UUID registrationId, UUID userId, UUID eventId) throws RegistrationNotFoundByIdException, UserNotFoundByIdException, EventNotFoundByIdException {
+        Registration registrationToUpdate = getRegistrationById(registrationId);
+
+        User user = userService.getUserById(userId);
+        Event event = eventService.getEventById(eventId);
+
+        registrationToUpdate.setUser_id(user);
+        registrationToUpdate.setEvent_id(event);
+
+        return registrationRepository.save(registrationToUpdate);
+    }
+
+    @Override
+    public void deleteRegistrationById(UUID registrationId) throws RegistrationNotFoundByIdException {
+        getRegistrationById(registrationId);
+        registrationRepository.deleteById(registrationId);
+    }
+}
